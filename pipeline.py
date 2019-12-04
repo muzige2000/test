a5ccda2abdb26f701d5cf350144a6cc32e4cadc3
Carlos <muzige2000@gmail.com>
2019-12-04 15:10:51 +0900

    Update pipeline.py

--- pipeline.py
+++ pipeline.py
@@ -1,6 +1,5 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
-from airflow.contrib.operators.kubernetes_pod_operator import KubernetesPodOperator
 from airflow.contrib.operators.dataproc_operator import DataProcSparkOperator, DataprocClusterCreateOperator, \
     DataprocClusterDeleteOperator, DataProcPySparkOperator
 from datetime import datetime, timedelta
@@ -20,25 +19,7 @@ default_args = {
 
 dag = DAG('pipeline', default_args=default_args, schedule_interval=None, catchup=False)
 
-gcpProjectName = "training-167208"
-gcpClusterName = "meetup-cluster"
-gcpRegion = "global"
-gcpZone = "europe-west1-d"
-# gcpDataStorage = "gs://meetup-bucket"
-gcpJar = "g3://datalabs.devops/godomall/gelatolab-jobs_2.12-0.1.jar"
-# gcpInitActions = 'gs://datlinq/dev/jobs/install-sql-proxy.sh'
-
 start = DummyOperator(task_id='run_this_first', dag=dag)
 end = DummyOperator(task_id='run_this_end', dag=dag)
-
-
-# etl_facebook = DataProcSparkOperator(
-#     task_id='t1_godomall',
-#     execution_timeout=timedelta(minutes=30),
-#     cluster_name=gcpClusterName,
-#     dataproc_spark_jars=[gcpJar],
-#     main_class='t1.BackupGodomallDB',
-#     arguments=["aaa", "bbb"],
-#     dag=dag)
 
 start >> end
